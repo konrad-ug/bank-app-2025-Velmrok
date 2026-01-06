a96a1dbbe657495ff35a17e93b9e61b9807f97f0
Maciej Spławiński <splawis3@gmail.com>
2026-01-06 15:40:50 +0100

    feature 18

--- src/company_account.py
+++ src/company_account.py
@@ -1,4 +1,9 @@
+from flask.cli import load_dotenv
 from src.base_account import BaseAccount
+from datetime import date
+import requests
+import os
+load_dotenv()
 class CompanyAccount(BaseAccount):
     def __init__(self, company_name, nip):
         super().__init__()
@@ -18,3 +23,19 @@ class CompanyAccount(BaseAccount):
             bank.transfer(self, amount)
             return True
         return False
+    def validate_nip_existance(self):
+        nip_str = str(self.nip)
+        if(nip_str == "Invalid"):
+            return False
+        current_date = date.today().isoformat()
+        mf_url = os.environ.get("BANK_APP_MF_URL", "https://wl-test.mf.gov.pl/")
+        response = requests.get(f"{mf_url}/api/search/nip/{nip_str}?date={current_date}")
+        if response.status_code != 200:
+            return False
+        data = response.json()
+        if data is None or 'statusVat' not in data:
+            return False
+        if data['statusVat'] != "Czynny":
+            raise ValueError("Company not registered!!")
+        print("NIP is active")
+        return True
