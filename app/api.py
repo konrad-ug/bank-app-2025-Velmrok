17e2c14ed22aec70cb66a32527cb5c605d1b6343
Maciej Spławiński <splawis3@gmail.com>
2026-01-28 14:21:12 +0100

    Feature 20

--- app/api.py
+++ app/api.py
@@ -6,10 +6,12 @@ from flask import Flask, app
 from src.account_registry import AccountRegistry
 from src.personal_account import PersonalAccount
 from app.routes.accounts import accounts_bp
+from src.mongo_accounts_repository import MongoAccountsRepository
 
 def create_app():
     app = Flask(__name__)
     app.registry = AccountRegistry()
+    app.accounts_repository = MongoAccountsRepository()
     app.config["TESTING"] = True 
     app.register_blueprint(accounts_bp)
     return app
